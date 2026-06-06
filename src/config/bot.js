433f23d71cb2b469898d4bb68f5a78f5e0cce5b4
SSSSSRQRQFAQR <stormggroblox@gmail.com>
2026-06-06 05:09:03 +0100

    Update bot activity name to 'RP Dragon V2 Community ❤️'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "RP Dragon V2 Community ❤️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
